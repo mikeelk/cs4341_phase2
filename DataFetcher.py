1f9e3fee3867eb30185f694aeba37e76f2e4cebd
Mike Elkinson <mike.elkinson@ticura.io>
2025-12-04 20:44:27 -0500

    data fetcher class

--- DataFetcher.py
+++ DataFetcher.py
@@ -1,35 +1,101 @@
 from pytrends.request import TrendReq
-import yfinance as yf
-import pandas
-import time
-import random
+from finvizfinance.quote import finvizfinance
+import requests
+
+"""
+Pulls data on queried stock from google trends, finviz, and reddit 
+"""
+class DataFetcher:
+
+    #init data fetcher object for a specific stock (ticker_symbol)
+    def __init__(self, ticker_symbol):
+        self.ticker_symbol = ticker_symbol
+
+    """
+    Queries google trends api to fetch recent popularity data on requested stock
+
+    Returns: DataFrame of requested trends data
+    """ 
+    def fetch_trends(self): 
+        pytrends = TrendReq(hl='en-US', tz=360) 
+        kw_list = [self.ticker_symbol] 
+        
+        pytrends.build_payload(kw_list, cat=0, timeframe='today 1-m', geo='', gprop='') 
+        
+        data = pytrends.interest_over_time()
+        
+        return data
 
 
-def fetch_yfin():
-    ticker_symbol = 'AAPL'
+    """
+    Queries FinViz api to fetch recent news headlines and short information related to requested stock
+    headlines can be analyzed for sentiment, and short info can reveal how the public views the stock
 
-    data = yf.download(ticker_symbol, start='2024-01-01', end='2024-12-31')
+    Datapoints:
+        Short Float: % of shares which are currently short sold
+        Short Ratio: Days needed for short sellers to buy back position
+        Short Interest: Total number of short sold shares
+        Rel Vol: FinViz attention metric
+        Volatility W/M: weekly/monthly volatility
+        ATR(14): another volatilty metric
+        Change: stock price change for today only
 
-    print(data.head())
+    Returns: DataFrame news headlines(with links), list of short info
+    """ 
 
-def fetch_trends(): 
-    pytrends = TrendReq(hl='en-US', tz=360) 
-    kw_list = ["TSLA"] 
+    def fetch_finviz(self):
+        
+        stock = finvizfinance(self.ticker_symbol.lower()) #finviz object for stock
+
+        news = stock.ticker_news() #get news headlines
+
+        fun = stock.ticker_fundament() #get the other datapoints
+        print(fun)
+        data = [fun["Short Float"], fun["Short Ratio"], fun["Short Interest"], fun["Rel Volume"], fun["Change"], fun["Volatility W"], fun["Volatility M"], fun["ATR (14)"]]
+
+        return news, data
+
+
+
+
+    """
+    Queries reddit public json endpoint over HTTP and grabs posts about queried stock
+
+    returns json object of posts, with text
+    """
+    def fetch_reddit(self):
+
+        #http headers
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        }
+        
+        url = f"https://www.reddit.com/search.json?q={self.ticker_symbol}&limit=30" #url
+
+        data = requests.get(url, headers=headers).json() #make request and get json of body
+        
+        return data
+
+
     
-    pytrends.build_payload(kw_list, cat=0, timeframe='today 1-m', geo='', gprop='') 
-    
-    data = pytrends.interest_over_time()
-    
-    print(data)
 
 def main():
-    fetch_yfin()
+
+    client = DataFetcher("AAPL")
+
+    print(client.fetch_finviz()) #test functions here
 
 
-
+    
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
 
---
+++ DataParser.py
@@ -0,0 +1,3 @@
+
+class DataParser:
+    pass
